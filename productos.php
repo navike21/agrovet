e25966474eab8e551f4d089ae20f0a21cdf7b7d7
navike21 <ivan@navike21.com>
2019-02-17 02:25:05 -0500

    añadiendo titulo

--- productos.php
+++ productos.php
@@ -32,7 +32,8 @@ if ($slug_url_grupo_prod_id != "") {
 </section>
 <section class="triangle section_top_center products">
 <?php if ( have_posts() ) : the_post(); ?>
-	<h2><?php the_title();?></h2>
+	<!-- <h2><?php //the_title();?></h2> -->
+	<h2>ADITIVOS PARA ALIMENTOS BALANCEADOS</h2>
 	<div class="section_top_center w_80 grupo_producto">
 	<!-- Listado de Grupos de Categorías -->
 	<?php
