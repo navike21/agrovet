544f07f87e5c4196bf6b216d6e874a7fc8f6e902
José Iván Chapoñán Casiano <ivan@navike21.com>
2019-04-09 02:21:04 -0500

    jumper jquery

--- assets/js/agrovet.js
+++ assets/js/agrovet.js
@@ -319,8 +319,10 @@ $(document).ready(function() {
 		a ++;
 	});
 
-	$(".sub-menu > li > a").on('click', function () { 
-		let href = $(this).hash;
-		alert(href);
+	$(".sub-menu > li > a").on('click', function (e) { 
+		e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 500);
 	})
 });
